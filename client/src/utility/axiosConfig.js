9a8c743c4baf2a126f2ad2a44234f82201472d17
Goku-121 <pranto1320@gmail.com>
2026-06-15 21:11:39 +0600

    fix: read token from both localStorage and cookies in interceptor

--- client/src/utility/axiosConfig.js
+++ client/src/utility/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5020";
 
@@ -7,7 +8,7 @@ if (API_BASE) {
 }
 
 axios.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token') || Cookies.get('token');
     if (token) config.headers['token'] = token;
     return config;
 });
